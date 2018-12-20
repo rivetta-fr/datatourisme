64a0b367a4319bea4061b53e1da9e12260e481c6
Rivetta.fr <claudio@rivetta.fr>
2018-12-20 11:29:01 +0100

    Modified themes extraction

--- jsonld2csv.py
+++ jsonld2csv.py
@@ -31,32 +31,42 @@ with open(re.sub(r'json.*', 'csv', sys.argv[1]), 'w') as csvfile:
         for e in data['@graph']:
             # extraction des éléments du graphe à sortir en CSV
             uri = ldget(e, ['@id'])
-            label = ldget(e, ['rdfs:label'])
-            comment = ldget(e, ['rdfs:comment'])
-            startdate = ldget(e, [':takesPlaceAt', ':startDate', '@value'])
-            enddate = ldget(e, [':takesPlaceAt', ':endDate', '@value'])
-            geo = ldget(e, [':isLocatedAt', 'schema:geo'])
+            labelobj = ldget(e, ['rdfs:label'])
+            labellang = ldget(labelobj, ['@language'])
+            label = ""
+            if labellang == 'fr':
+                label = ldget(labelobj, ['@value'])
+            commentobj = ldget(e, ['rdfs:comment'])
+            commentlang = ldget(commentobj, ['@language'])
+            comment = ""
+            if commentlang == 'fr':
+                comment = ldget(commentobj, ['@value'])
+            startdate = ldget(e, ['takesPlaceAt', ':startDate', '@value'])
+            enddate = ldget(e, ['takesPlaceAt', ':endDate', '@value'])
+            geo = ldget(e, ['isLocatedAt', 'schema:geo'])
             lat = ldget(geo, ['schema:latitude', '@value'])
             lon = ldget(geo, ['schema:longitude', '@value'])
-            addr = ldget(e, [':isLocatedAt', 'schema:address'])
+            addr = ldget(e, ['isLocatedAt', 'schema:address'])
             street = ldget(addr, ['schema:streetAddress'])
             cp = ldget(addr, ['schema:postalCode'], '')
-            city = ldget(addr, [':hasAddressCity', 'rdfs:label'])
-            if city:
-                city = city[0]['@value']
-            insee = ldget(addr, [':hasAddressCity', ':insee'])
-            last_update = ldget(addr, [':lastUpdate'])
+            city = ldget(addr, ['schema:addressLocality'])
+            insee = ldget(addr, ['hasAddressCity', 'insee'])
+            last_update = ldget(addr, ['lastUpdate'])
             event_type = '/'.join(ldget(e, ['@type']))
-            email = ldget(e, [':hasContact', 'schema:email'])
-            web = ldget(e, [':hasContact', 'foaf:homepage'])
-            tel = ldget(e, [':hasContact', 'schema:telephone'])
+            email = ldget(e, ['hasContact', 'schema:email'])
+            web = ldget(e, ['hasContact', 'foaf:homepage'])
+            tel = ldget(e, ['hasContact', 'schema:telephone'])
 
-            themes = ldget(e, [':hasTheme', 'rdfs:label'], None)
+            themes = ldget(e, ['hasTheme'])
             event_theme = ''
-            if themes:
-                for t in themes:
-                    if t['@language'] == 'fr':
-                        event_theme = event_theme + t['@value'] + ', '
+            if themes is not None:
+                if 'rdfs:label' in themes and '@language' in themes['rdfs:label'] and  themes['rdfs:label']['@language'] == 'fr' :
+                    event_theme = event_theme + themes['rdfs:label']['@value'] + ', '
+                else:
+                    if len(themes) > 0:
+                        for t in themes :
+                            if 'rdfs:label' in t and '@language' in t['rdfs:label'] and t['rdfs:label']['@language'] == 'fr':
+                                event_theme = event_theme + t['rdfs:label']['@value'] + ', '
                 event_theme = event_theme[:-2]
 
             # écriture dans le fichier CSV
